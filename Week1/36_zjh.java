56d6ac051b4460ff5336fb9ef4f59daeadcb3d29
YoKeing <53143388+YoKeing@users.noreply.github.com>
2020-11-04 17:53:33 +0800

    Update 36_zjh.java

--- Week1/36_zjh.java
+++ Week1/36_zjh.java
@@ -39,3 +39,16 @@ class Solution {
     }
 }
 
+/**
+ * 120. 三角形最小路径和 c解决
+ */
+
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
+    for(int i = triangleSize - 2; i > -1; i--){
+        for(int j = 0; j < triangleColSize[i]; j++){
+            triangle[i][j] += 
+            triangle[i + 1][j] > triangle[i + 1][j + 1] ? triangle[i + 1][j + 1] : triangle[i + 1][j];
+            }
+        }
+    return triangle[0][0];
+}
