68231bb76567afe8b4201e775ebd6b07f7f9d8c8
AlienWare-R <2937015051@qq.com>
2020-11-08 10:48:38 +0800

    2

--- Week1/26_pc.cpp
+++ Week1/26_pc.cpp
@@ -1,3 +1,4 @@
+//1、存在连续三个奇数的数组
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
@@ -13,3 +14,21 @@ public:
         return false;
     }
 }; 
+
+//2、所有奇数长度子数组的和
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum = 0;
+        int t = 0;
+        for(int i = 0; i < arr.size(); ++i){
+            sum = 0;
+            for(int j = i; j < arr.size(); ++j){
+                sum += arr[j];
+                if((j-i)%2 == 0)
+                    t += sum;
+            }
+        }
+        return t;
+    }
+};
