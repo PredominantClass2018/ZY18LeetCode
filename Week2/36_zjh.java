fc3e3dc064238f4a2d30f7e2242a6e1df63bac3a
YoKeing <53143388+YoKeing@users.noreply.github.com>
2020-11-10 11:00:02 +0800

    Update 36_zjh.java

--- Week2/36_zjh.java
+++ Week2/36_zjh.java
@@ -20,3 +20,24 @@ class Solution {
         return false;
     }
 }
+
+/**
+ * 1535. 找出数组游戏的赢家
+ */
+class Solution {
+    public int getWinner(int[] arr, int k) {
+
+        int win = 0;
+        for(int i = 1; i < arr.length && win < k; i++){
+
+            if(arr[i] < arr[0]){
+                win++;
+            }else{
+                arr[0] = arr[i];
+                win= 1;
+            }
+        }
+        return arr[0];
+    }
+}
+
