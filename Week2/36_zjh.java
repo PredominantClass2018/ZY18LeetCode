97b2ca0130ad64832615638a1769d8a1bec9436e
YoKeing <53143388+YoKeing@users.noreply.github.com>
2020-11-10 11:20:53 +0800

    Update 36_zjh.java

--- Week2/36_zjh.java
+++ Week2/36_zjh.java
@@ -41,3 +41,41 @@ class Solution {
     }
 }
 
+
+/**
+ * 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    List<List<Integer>> list = new ArrayList();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        levelTraversal(root, 0);
+        return list;
+    }
+
+    public void levelTraversal(TreeNode root , int k){
+        if(root != null){
+            if(list.size() <= k )
+                list.add(new ArrayList());
+            list.get(k).add(root.val);
+            levelTraversal(root.left, k + 1);
+            levelTraversal(root.right, k + 1);
+        }
+    }
+
+
+
+}
+
+
+
