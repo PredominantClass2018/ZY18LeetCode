2a1eb23056548ee00adb480e5fad8c2fea489062
YoKeing <53143388+YoKeing@users.noreply.github.com>
2020-11-22 17:48:05 +0800

    Update 36_zjh.java

--- Week3/36_zjh.java
+++ Week3/36_zjh.java
@@ -52,4 +52,56 @@ class Solution {
     }
 }
 
+/**
+ * 547. 朋友圈
+ */
+class Solution {
+    public int findCircleNum(int[][] M) {
+        int N = M.length;
+        UnionSearchSet unionSearchSet = new UnionSearchSet(N);
+        for (int row = 0; row < N; ++row) {
+            for (int column = row + 1; column < N; ++column) { // 不需要从0开始判断
+                if (M[row][column] == 1) unionSearchSet.union(column, row);
+            }
+        }
+        return unionSearchSet.countSetNumber();
+    }
+}
+
+class UnionSearchSet{
+    private int[] unionsearch;
+
+    UnionSearchSet(int n){
+        unionsearch = new int[n];
+        for(int i = 0; i < n; i++){
+            unionsearch[i] = i;
+        }
+    }
+
+    void union(int person1, int person2){
+        int x = find(person1);
+        int y = find(person2);
+        if(x != y){
+            unionsearch[x] = y;
+        }
+    }
+
+    int find(int person){
+        if(person == unionsearch[person])
+            return person;
+        return unionsearch[person] = find(unionsearch[person]);
+    }
+
+    public int countSetNumber(){
+        int count = 0;
+        for(int i = 0; i < unionsearch.length; i++){
+            if(unionsearch[i] == i)
+                count++;
+        }
+        return count;
+    }
+}
+
+
+
 
