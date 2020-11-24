d6b34619f5fbca335bd6a84a1ecbd37849de28e7
wxpppp <1534744161@qq.com>
2020-11-24 16:07:09 +0800

    11.2

--- Week3/02_wwj.c
+++ Week3/02_wwj.c
@@ -2,7 +2,30 @@
 
 /*5最长回文子串*/
 
-null 
+char * longestPalindrome(char * s){
+    if(strlen(s)==0||strlen(s)==1) return s;
+    int i,start,left,right,count,len;
+    start = len =0;
+    for(i=0;s[i]!='\0';i+=count){
+        count = 1;
+        left=i-1;
+        right = i+1;
+        while(s[right]!='\0'&&s[i]==s[right]){ 
+            right++;
+            count++;
+        }
+        while(left>=0 && s[right]!='\0' && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        if(right-left-1>len){
+            start = left+1;
+            len = right-left-1;
+        }
+    }
+    s[start + len] = '\0';    
+    return s + start;
+}
 
 /*1025除数博弈*/
 bool divisorGame(int N){
