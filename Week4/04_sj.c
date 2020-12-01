38734f5ed2dfe865a348c9ccd4d2a4e1935e9b23
SJ-Ruby <66662493+SJ-Ruby@users.noreply.github.com>
2020-12-01 15:02:13 +0800

    Update 04_sj.c

--- Week4/04_sj.c
+++ Week4/04_sj.c
@@ -54,3 +54,58 @@ circle-and-rectangle-overlapping
     int dy = y1 > y_center ? y1 - y_center : y2 < y_center ? y_center - y2 : 0;
     return dx * dx + dy * dy <= radius * radius;
 }*/
+
+
+
+/****************
+rotate-array
+****************/
+
+/*********  version 1 暴力算法 时间复杂度O(n*k)    *********/  
+/*void rotate(int* nums, int numsSize, int k){
+    int t;
+    for (int i = 0; i < k; ++i) {
+        t = nums[numsSize - 1];
+        for (int j = numsSize - 2; j >= 0; j--)
+            nums[j + 1] = nums[j];
+        nums[0] = t;
+    }
+}*/
+
+/*********  version 2 额外的数组 时间复杂度O(n)    *********/  
+//用一个额外的数组来将每个元素放到正确的位置上，原本数组里下标为 i的把它放到 (i+k)\%数组长度(i+k)%数组长度 的位置。然后把新的数组拷贝到原数组中
+/*void rotate(int* nums, int numsSize, int k){
+    int* array;
+    array=(int *)malloc(numsSize*sizeof(int));
+    for(int i =0;i<numsSize;++i)
+        array[(i+k)%numsSize]=nums[i];
+    for(int i =0;i<numsSize;++i)
+        nums[i]=array[i];
+}*/
+
+/*********  version 3 环状替换 时间复杂度O(n)    *********/  
+//假设，数组里我们有 n 个元素并且 k 是要求移动n/k的次数。
+//更进一步，假设 n%k=0 。第一轮中，所有移动数字的下标 i 满足 i%k==0 。这是因为每跳 k 步，只会到达相距为 k 个位置下标的数。
+//每一轮，移动 n/k个元素。下一轮中，移动满足 i%k==1 的位置的数。这样的轮次会一直持续到再次遇到 i%k==0 的地方为止，此时 i=k 。
+//此时在正确位置上的数字共有 k× n/k =n 个。因此所有数字都在正确位置上。
+/*void rotate(int* nums, int numsSize, int k){
+    k=k%numsSize;
+    int count=0;
+    for(int start=0;count<numsSize;start++){
+        int current=start;
+        int prev=nums[start];
+        do{
+            int next=(current+k)%numsSize;
+            int temp=nums[next];
+            nums[next]=prev;
+            prev=temp;
+            current=next;
+            count++;
+        }while(start!=current);
+    }
+}*/
+
+
+/*********  version 4 使用反转 时间复杂度O(n)    *********/ 
+//基于事实：当旋转数组 k 次，k%n 个尾部元素会被移动到头部，剩下的元素会被向后移动。
+//建议C++ JAVA
