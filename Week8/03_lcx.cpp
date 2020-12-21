99cbce78193b0a34b40fcdac46ebc25543fbd673
1DreamWake1 <1127128563@qq.com>
2020-12-21 22:56:58 +0800

    lcx

--- Week8/03_lcx.cpp
+++ Week8/03_lcx.cpp
@@ -43,23 +43,15 @@ public:
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-		if(nums.size() == 1)
-			return nums[0];
-		int left = 0, right = nums.size() - 1, mid = 0;
-		if(nums[right] > nums[0])
-			return nums[0];
-		while(left <= right){
-			mid = left + (right - left) / 2;
-			if(nums[mid] > nums[mid + 1])
-				return nums[mid + 1];
-			else if(nums[mid] < nums[mid - 1])
-				return nums[mid];
-			if(nums[mid] > nums[0])
-				left = mid + 1;
-			else
-				right = mid - 1;
-		}
-		return -1;
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] < nums[right])
+                right = middle;
+            else
+                left = middle + 1;
+        }
+        return nums[left];
     }
 };
 
